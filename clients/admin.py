ac5c5c6f371a10546381544ffedeb9e5e8755618
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-08-31 21:04:58 -0300

    feat: added Clients admin

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from clients.models import Client
 
-admin.site.register(Client)
+class Clients(admin.ModelAdmin):
+    list_display = ['id', 'name', 'address', 'country', 'city']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'country']
+    list_per_page = 20
+    
+
+admin.site.register(Client, Clients)
